7d2126097f23b607654d854c1392c1c167634849
Minju Kim <kmj673@gmail.com>
2022-02-03 02:13:34 +0000

    added submit button to get the string of the word
    Relates #1

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+"use strict";
 const keyboards = document.querySelectorAll("kbd");
+const letterContainer = document.querySelector(".letter-container");
+const submitButton = document.querySelector("button");
+const todayWord = "shard";
 
 function handleKey(event) {
-  console.log(event.target.textContent);
+  return event.target.textContent;
+}
+function handleLetter(key) {
+  let letter = document.createElement("span");
+  letter.textContent = key;
+  letterContainer.appendChild(letter);
 }
 
 keyboards.forEach((keyboard) => {
-  keyboard.addEventListener("click", handleKey);
+  keyboard.addEventListener("click", (event) => {
+    let clickedKey = handleKey(event);
+    handleLetter(clickedKey);
+  });
 });
+
+function handleSubmit() {
+  let letters = letterContainer.querySelectorAll("span");
+  let lettersArr = [];
+  letters.forEach((letter) => {
+    lettersArr.push(letter.textContent);
+  });
+  let word = lettersArr.join("");
+  console.log(word);
+}
+
+submitButton.addEventListener("click", handleSubmit);
